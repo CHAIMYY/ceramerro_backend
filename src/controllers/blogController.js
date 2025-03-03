dfcb71aa1fc5bea99dcd67c8c444661467422b28
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2025-03-03 12:36:59 +0000

    feat: added comments, users can add comment on any post in blog section

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -11,23 +11,60 @@ exports.createPost = async (req, res) => {
   }
 };
 
+exports.updatePost = async (req, res) => {
+  try {
+    const updatepost = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!updatepost)
+      return res.status(404).json({ message: "could not find post" });
+    res.json(updatepost);
+  } catch (err) {
+    res.status(500).json({ message: "failed updating post", error: err });
+  }
+};
 
-exports.updatePost = async (req, res) =>{
-try{
-const updatepost = await Blog.findByIdAndUpdate(req.params.id, req.body, ({new: true}));
-if(!updatepost) return res.status(404).json({message: 'could not find post'})
-res.json(updatepost);
-}catch(err){
-    res.status(500).json({message: "failed updating post", error:err});
-}
-}
-
-exports.deletePost = async (req, res) =>{
-    try{
+exports.deletePost = async (req, res) => {
+  try {
     const deletepost = await Blog.findByIdAndDelete(req.params.id, req.body);
-    if(!deletepost) return res.status(404).json({message: 'could not find post'})
-    res.json({message:"post deleted"});
-    }catch(err){
-        res.status(500).json({message: "failed deleting post", error:err});
+    if (!deletepost)
+      return res.status(404).json({ message: "could not find post" });
+    res.json({ message: "post deleted" });
+  } catch (err) {
+    res.status(500).json({ message: "failed deleting post", error: err });
+  }
+};
+
+exports.addComment = async (req, res) => {
+  try {
+    const post = await Blog.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ message: "Could not find the post" });
     }
+
+    const { text, postedBy } = req.body;
+
+    if (!text) {
+      return res.status(400).json({ message: "Comment text is required" });
     }
+
+    if (!postedBy) {
+      return res.status(400).json({ message: "User ID is required" });
+    }
+
+    const newComment = {
+      text,
+      postedBy,
+      createdAt: new Date(),
+    };
+
+    post.comments.push(newComment);
+    await post.save();
+
+    res.status(201).json(post);
+  } catch (err) {
+    console.error("Error adding comment:", err);
+    res.status(500).json({ message: "Failed to add comment" }); 
+  }
+};
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { createPost, updatePost, deletePost} = require('../controllers/blogController');
+const { createPost, updatePost, deletePost, addComment} = require('../controllers/blogController');
 
 router.post("/create", createPost); 
 router.put("/update/:id", updatePost);
 router.delete("/delete/:id", deletePost);
-
+router.post('/comment/:id',addComment )
 
 
 module.exports = router;
