4f4f00e4072f885208c57a5501b3a2f4d22b6b0e
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2025-03-03 10:12:24 +0000

    CER-72 feat: enable product details modification

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -12,7 +12,16 @@ try{
     return res.status(500).error({message:'failed creating a product' , error:err})
 }
 }
+exports.updateProduct =async (req, res)=>{
 
+    try{
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
+        if(!product) return res.status(404).json({message:'could not find the product'});
+        res.json(product);
+    }catch(err){
+        return res.status(500).error({message:'failed updating a product' , error:err})
+    }
+    }
 
 
 
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { createProduct} = require("../controllers/productController");
+const { createProduct, updateProduct} = require("../controllers/productController");
 
 router.post('/create', createProduct);
-
+router.put('/update/:id', updateProduct);
 
 module.exports = router;
