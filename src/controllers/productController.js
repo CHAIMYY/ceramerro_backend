002ccc844a699e00db99b31858993bdc2b31e6c9
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2025-03-03 10:23:20 +0000

    CER-73 feat: implement product deletion

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -12,16 +12,29 @@ try{
     return res.status(500).error({message:'failed creating a product' , error:err})
 }
 }
-exports.updateProduct =async (req, res)=>{
 
-    try{
-        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
-        if(!product) return res.status(404).json({message:'could not find the product'});
-        res.json(product);
-    }catch(err){
+exports.updateProduct =async (req, res)=>{
+ try{
+   const product = await Product.findByIdAndUpdate(req.params.id, req.body, {new: true});
+   if(!product) return res.status(404).json({message:'could not find the product'});
+    res.json(product);
+}catch(err){
         return res.status(500).error({message:'failed updating a product' , error:err})
-    }
-    }
+}
+}
+
+exports.deleteProduct =async (req, res)=>{
+    try{
+      const product = await Product.findByIdAndDelete(req.params.id, req.body);
+      if(!product) return res.status(404).json({message:'could not find the product'});
+       res.status(201).json({message: 'product deleted succefully'});
+   }catch(err){
+           return res.status(500).error({message:'failed deleting a product' , error:err})
+   }
+   }
+
+
+
 
 
 
