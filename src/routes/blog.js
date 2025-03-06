7c630d5899c669a1e20c5609ad6015fce3451453
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2025-03-06 10:16:13 +0000

    feat: added blog routes

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { createPost, updatePost, deletePost, addComment} = require('../controllers/blogController');
+const { authenticate }  = require('../middleware/authentication');
+const { createPost, updatePost, deletePost, addComment, deleteComment} = require('../controllers/blogController');
 
 router.post("/create", createPost); 
 router.put("/update/:id", updatePost);
 router.delete("/delete/:id", deletePost);
-router.post('/comment/:id',addComment )
+// router.post('/comment/:id',addComment );
+// router.delete('/posts/:postId/comments/:commentId', authenticateJWT, deleteComment);
 
 
 module.exports = router;
