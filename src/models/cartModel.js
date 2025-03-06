4a69ba8f472ae633f6a9b836862eb3af575451c7
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2025-03-06 15:27:16 +0000

    feat: added route for removing product from cart

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const CartSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    articles: [{ product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' }, quantity: Number }]
+    articles: [{
+      product: {
+       type: mongoose.Schema.Types.ObjectId,
+       ref: 'Product'
+     },
+      quantity: Number
+     }]
 });
 
 const Cart = mongoose.model('Cart' , CartSchema);
