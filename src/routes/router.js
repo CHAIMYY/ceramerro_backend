214ee1bb441baf14f8c6198ebcc0bda2de54eb2b
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2025-03-06 10:17:09 +0000

    feat: added new routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,10 +4,14 @@ const router = express.Router();
 const userRouter = require("./user");
 const productRouter = require("./product");
 const blogRouter = require("./blog")
+const commentRouter = require("./comment")
+const adminRouter = require("./admin")
 
 router.use("/user", userRouter);
 router.use("/product", productRouter);
 router.use("/blog", blogRouter)
+router.use("/comment", commentRouter)
+router.use("/admin", adminRouter)
 
 
 module.exports = router;
