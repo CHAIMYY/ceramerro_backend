bfc032cbd5d87a9ac041881ad8c2cde1b037ae25
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2025-03-06 15:22:37 +0000

    feat: added removeFromCart function that removes a product from the cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -34,13 +34,13 @@ exports.updateProductQuantity = async (req, res) => {
         if (cart) {
             
             const productInCart = cart.articles.find(item => item.product === product);
-            
+
             if (productInCart) {
              
                 productInCart.quantity = quantity;
 
                 if (quantity <= 0) {
-                    cart.articles = cart.articles.filter(item => article.product._id.toString() !== product);
+                    cart.articles = cart.articles.filter(item => item.product._id.toString() !== product);
                 }
 
                 await cart.save();
@@ -57,3 +57,41 @@ exports.updateProductQuantity = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+exports.removeFromCart = async (req, res) => {
+    try {
+      const { user, produit } = req.body;
+      let cart = await Cart.findOne({ user: user });
+      
+      if (!cart) {
+        return res.status(404).json({ message: 'Cart not found' });
+      }
+      
+      const produitId = produit;
+      console.log("Product ID to remove:", produitId);
+      
+     
+      const productField = cart.articles[0].product ? 'product' : '_id';
+      
+    
+      const updatedArticles = cart.articles.filter(item => {
+       
+        const itemId = item[productField] ? item[productField].toString() : item[productField];
+        return itemId !== produitId;
+      });
+      
+      console.log("Updated articles:", updatedArticles);
+      
+      if (updatedArticles.length === cart.articles.length) {
+        return res.status(404).json({ message: 'Product not found in cart' });
+      }
+      
+      cart.articles = updatedArticles;
+      await cart.save();
+      
+      return res.status(200).json({ message: 'Product removed successfully', cart });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error.message });
+    }
+  };
