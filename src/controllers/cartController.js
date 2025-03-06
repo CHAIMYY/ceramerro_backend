5d1ad882ab1190ea00744eb7f998a97017d7feb1
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2025-03-06 14:12:34 +0000

    feat: added updateProductQuantity function that updates the quantity of any product in the cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -25,4 +25,35 @@ exports.addToCart = async (req, res) => {
     }
 };
 
+exports.updateProductQuantity = async (req, res) => {
+    try {
+        const { user, product, quantity } = req.body;
+      
+        let cart = await Cart.findOne({ user: user }).populate('articles.product').exec();
 
+        if (cart) {
+            
+            const productInCart = cart.articles.find(item => item.product === product);
+            
+            if (productInCart) {
+             
+                productInCart.quantity = quantity;
+
+                if (quantity <= 0) {
+                    cart.articles = cart.articles.filter(item => article.product._id.toString() !== product);
+                }
+
+                await cart.save();
+
+                return res.status(200).json(cart);  
+            } else {
+                return res.status(404).json({ message: 'Product not found in cart' });
+            }
+        } else {
+            return res.status(404).json({ message: 'Cart not found' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+};
