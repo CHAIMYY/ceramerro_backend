75358f665ef2edb8ba646aa47a8a58323367cc75
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2025-03-06 14:19:13 +0000

    feat: added updateProductQuantity route for the function that updates the quantity of any product in the cart

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const { authenticate }  = require('../middleware/authentication');
-const { addToCart } = require('../controllers/cartController');
+const { addToCart, updateProductQuantity, removeFromCart} = require('../controllers/cartController');
 
 
-router.post("/create", addToCart)
+router.post("/create", addToCart);
+router.put("/quantity", updateProductQuantity);
+router.delete("/remove", removeFromCart)
+
+
 
 module.exports = router;
