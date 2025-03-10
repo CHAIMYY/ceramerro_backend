5b7ee22958ad312ee1e951c5b759afb4cb097561
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2025-03-10 14:36:06 +0000

    fix : fixed the blog route

--- src/routes/router.js
+++ src/routes/router.js
@@ -7,7 +7,7 @@ const blogRouter = require("./blog")
 const commentRouter = require("./comment")
 const adminRouter = require("./admin")
 const cartRouter = require("./cart")
-
+const artisanRouter = require("./artisan")
 
 router.use("/user", userRouter);
 router.use("/product", productRouter);
@@ -15,6 +15,6 @@ router.use("/blog", blogRouter)
 router.use("/comment", commentRouter)
 router.use("/admin", adminRouter)
 router.use("/cart", cartRouter)
-
+router.use("/artisan", artisanRouter)
 
 module.exports = router;
