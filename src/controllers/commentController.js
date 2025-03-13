296e9e5ec45bf7fb4fa8672c27fced98683eddb6
Chaimaa HOUAS <144816252+CHAIMYY@users.noreply.github.com>
2025-03-13 12:52:43 +0000

    fix: fixed deleteComment function

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -10,64 +10,59 @@ exports.addComment = async (req, res) => {
       return res.status(404).json({ message: "Could not find the post" });
     }
 
-  const { text } = req.body; 
+    const { text } = req.body;
 
-    const postedBy = req.user._id
+    const postedBy = req.user._id;
 
     // console.log("===>"+postedBy);
 
     const postId = req.params.id;
     // console.log("2====>",postId);
     // console.log(text);
-    
-
 
     if (!text) {
       return res.status(400).json({ message: "Comment text is required" });
     }
-    
+
     if (!postedBy) {
       return res.status(400).json({ message: "User ID is required" });
     }
-   
+
     const newComment = new Comment({
       text,
       postedBy,
       postId,
     });
-   
 
     await newComment.save();
-    
+
     res.status(201).json(newComment);
     // console.log(newComment);
-    
   } catch (err) {
     console.error("Error adding comment:", err);
     res.status(500).json({ message: "Failed to add comment" });
   }
 };
 
-
 exports.deleteComment = async (req, res) => {
   try {
-    const id = req.params.id;
+    const id = req.body.id;
     // console.log("idddddd",id);
-    
-    const userId = req.body;
-    // console.log("useeeeeeer",userId.Id);
-    
+
+    const userId = req.user._id;
+    // console.log("useeeeeeer",userId.toString());
 
     const comment = await Comment.findById(id);
     // console.log("commmeeentarioooos",comment.postedBy.toString());
-    
 
     if (!comment) {
       return res.status(404).json({ message: "Comment not found" });
     }
-  
-    if (comment.postedBy.toString() !== userId.Id) {
-      return res.status(403).json({ message: "You are not authorized to delete this comment" });
+
+    if (comment.postedBy.toString() !== userId.toString()) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to delete this comment" });
     }
 
     await Comment.findByIdAndDelete(id);
